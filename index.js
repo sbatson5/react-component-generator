021e938e43ddc7954899443293b5a33eec6758a5
Scott Batson <sjbats@email.com>
2019-03-10 12:58:41 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const minimist = require('minimist')
+
+module.exports = () => {
+  const args = minimist(process.argv.slice(2));
+  const params = Object.assign({}, args);
+  delete params._;
+
+  const cmd = args._[0];
+  const file = args._[1];
+  const fileName = args._[2];
+
+  const MAPPED_KEYS = [
+    {
+      name: 'generate',
+      aliases: ['generate', 'gen', 'g', 'new']
+    }, {
+      name: 'delete',
+      aliases: ['delete', 'del', 'd', 'destroy']
+    }
+  ];
+
+  let command = MAPPED_KEYS.find((mappedKey) => {
+    return mappedKey.aliases.includes(cmd);
+  });
+
+  switch (command.name) {
+    case 'generate':
+      require('./generate/file')(file, fileName, params);
+      break;
+    case 'delete':
+      console.log('we will delete stuff');
+      break;
+    default:
+      console.error(`"${cmd}" is not a valid command!`)
+      break;
+  }
+}
